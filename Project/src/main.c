106fabe61ce711757940cd1924b0a878b9fb341c
guzhaoyuan <guzhaoyuan14@gmail.com>
2019-01-19 19:47:19 -0500

    init repo

---
+++ Project/src/main.c
@@ -0,0 +1,55 @@
+#include "stm32f4xx_it.h"
+
+void Delay(__IO uint32_t nCount)
+{
+  while(nCount--)
+  {
+  }
+}
+
+int main(){
+	
+	GPIO_InitTypeDef  GPIO_InitStructure;
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+
+  /* Configure PD12, PD13, PD14 and PD15 in output pushpull mode */
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13| GPIO_Pin_14| GPIO_Pin_15;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  GPIO_Init(GPIOD, &GPIO_InitStructure);
+
+  while (1)
+  {
+    /* PD12 to be toggled */
+    GPIO_SetBits(GPIOD, GPIO_Pin_12);
+    
+    /* Insert delay */
+    Delay(0x3FFFFF);
+    
+    /* PD13 to be toggled */
+    GPIO_SetBits(GPIOD, GPIO_Pin_13);
+    
+    /* Insert delay */
+    Delay(0x3FFFFF);
+  
+    /* PD14 to be toggled */
+    GPIO_SetBits(GPIOD, GPIO_Pin_14);
+    
+    /* Insert delay */
+    Delay(0x3FFFFF);
+    
+    /* PD15 to be toggled */
+    GPIO_SetBits(GPIOD, GPIO_Pin_15);
+    
+    /* Insert delay */
+    Delay(0x7FFFFF);
+    
+    GPIO_ResetBits(GPIOD, GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_14|GPIO_Pin_15);
+    
+    /* Insert delay */
+    Delay(0xFFFFFF);
+  }
+	
+}
